24aff9f150c386ec88705d0bde43d8e0af9afc33
Ruma Abhinav <ruma.abhinav2017@gmail.com>
2018-06-22 21:00:01 +1000

    initial commit

--- common.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-basedir=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-function apcEncrypt () {
-    pubkey=${pubkey:-$basedir/apc/public_uat}
-    input=$1
-    echo -n $input | openssl rsautl -encrypt -pubin -inkey $pubkey | base64 -w0
-}
